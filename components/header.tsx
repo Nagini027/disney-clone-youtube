dfd4ff88661cdd882821ca01f8679c2188f301d2
Nagini027 <nagalakshmis027@gmail.com>
2024-04-03 16:43:05 +0530

    updated files

---
+++ components/header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ThemeToggler } from "./ThemeToggler";
+import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
+
+function header() {
+  return (
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 ">
+      <Link href="/" className="mr-10">
+        <Image
+          src="https://links.papareact.com/a943ae"
+          alt="Disney Logo"
+          height={100}
+          width={120}
+          className="cursor-pointer invert-0 dark:invert"
+        />
+      </Link>
+
+      <div className="flex space-x-2">
+        {/* GenreDropdown */}
+        <GenreDropdown />
+        {/*SearchInput */}
+        <SearchInput />
+        {/* Themetoggler */}
+        <ThemeToggler />
+      </div>
+    </header>
+  );
+}
+
+export default header;
